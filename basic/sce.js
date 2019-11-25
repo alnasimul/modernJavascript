2a2de3fb01a088a4c81020e5c48ac4b1ce77cf53
alnasimul <alnasimul@gmail.com>
2019-11-25 22:29:48 +0600

    Corrected Short Circuit Evaluation like if else statement

--- basic/sce.js
+++ basic/sce.js
@@ -4,13 +4,24 @@ var a=0;
 
 a || (a=5);
 /*
-in that case first expression return false then second expression will be executed 
+in that case if first expression return false then second expression will be executed 
+first expression will not be executed
 on the other hand if first expression retrun true then first expression will be excuted 
-second expression will not be executed */
+second expression will not be executed 
+*/
+
 console.log(a);
 
 //- - - - - //
 
 var b = 0;
+
 b && (b=5)
+/*
+in that case if first expression return false then second expression will not be executed 
+only first expression will be excuted
+on the other hand if first expression retrun true then first expression will not be excuted 
+second expression will be executed
+ */
+
 console.log(b);
