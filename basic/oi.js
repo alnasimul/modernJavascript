96eccc92b467a60ed89a8842e8107a95b3762dca
alnasimul <alnasimul@gmail.com>
2019-12-02 01:12:55 +0600

    introduction of object adding and deleting property in object

---
+++ basic/oi.js
@@ -0,0 +1,27 @@
+// Introduction of Object
+
+var a = {
+    a:10,
+    b:"Hello World",
+}
+
+
+var e = {
+    a:10,
+    b:"Hello World",
+}
+
+var x = e
+
+x.a = 50;
+// adding property in e object
+
+e.c = "Hi";
+
+//deleting property in e object
+delete e.a;
+
+console.log(e);
+
+
+
