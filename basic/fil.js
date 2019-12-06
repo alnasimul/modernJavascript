bdcf9fadf39f85927c9206aa691f925b6df8515d
alnasimul <alnasimul@gmail.com>
2019-12-06 23:02:31 +0600

    All About for in loop related with object

---
+++ basic/fil.js
@@ -0,0 +1,12 @@
+// for in loop
+
+var manush = {
+    name:"Sadaf",
+    age:100,
+    gender:"M",
+    city:"Sylhet"
+}
+
+for(let item in manush){
+    console.log(`${item}: ${manush[item]}`);
+}
