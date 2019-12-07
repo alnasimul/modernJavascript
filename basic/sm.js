469542e845c8d99b7ffb6d38d57e45ff32a4e758
alnasimul <alnasimul@gmail.com>
2019-12-08 00:29:58 +0600

    string methods and string reverse

---
+++ basic/sm.js
@@ -0,0 +1,19 @@
+//String Method
+
+var s = "Quick Brown Fox Jump Over The Lazy Dog";
+
+//String Split
+
+var ss = s.split(" ");
+
+//String Reverse in terms of Array Form
+
+sr = ss.reverse();
+
+//String Join 
+
+var sj = ss.join(" ");
+
+//console.log(ss);
+console.log(sj);
+console.log(sr);
