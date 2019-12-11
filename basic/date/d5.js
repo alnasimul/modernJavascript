124e6ddb0eb2c04b0a83f6016be9bea4f59dd3f9
alnasimul <alnasimul@gmail.com>
2019-12-12 02:29:17 +0600

    setInterval

--- basic/date/d5.js
+++ basic/date/d5.js
@@ -2,4 +2,8 @@
 
 // setTimeOut function accepts two parameters 1.function as argument 2.Time as milisecond
 
-setTimeout(()=>{(console.log("Hello World"))},5000);
+//setTimeout(()=>{(console.log("Hello World"))},5000);
+
+var t = setTimeout(()=>{(console.log("Hello World"))},5000);
+
+clearTimeout(t);
---
+++ basic/date/d6.js
@@ -0,0 +1,5 @@
+// setInterval
+
+var t = setInterval(() => {
+    console.log("Hello World");
+}, 2000);
