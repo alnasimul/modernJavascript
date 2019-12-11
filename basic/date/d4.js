ad62a2737f80cdb0e6b55e02c10da1afabb0e454
alnasimul <alnasimul@gmail.com>
2019-12-12 02:12:36 +0600

    setTimeOut

---
+++ basic/date/d4.js
@@ -0,0 +1,12 @@
+// Setter
+
+var d = new Date();
+
+ d.setDate(13);
+ d.setMonth(5);
+ d.setFullYear(2018);
+ d.setHours(10);
+
+console.log(d.toDateString());
+console.log(d.toTimeString());
+
---
+++ basic/date/d5.js
@@ -0,0 +1,5 @@
+// setTimeOut 
+
+// setTimeOut function accepts two parameters 1.function as argument 2.Time as milisecond
+
+setTimeout(()=>{(console.log("Hello World"))},5000);
