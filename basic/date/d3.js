26d5d5b9da8b0cb38df5d34dbf7b82567eeff2dd
alnasimul <alnasimul@gmail.com>
2019-12-12 01:54:18 +0600

    Getter

---
+++ basic/date/d3.js
@@ -0,0 +1,15 @@
+// Getter
+
+var d = new Date();
+
+var v = d.getHours();
+var v1 = d.getMinutes();
+var v2 = d.getSeconds();
+var v3 = d.getFullYear()
+var v4 = d.getMonth();
+
+console.log(v);
+console.log(v1);
+console.log(v2);
+console.log(v3);
+console.log(v4);
